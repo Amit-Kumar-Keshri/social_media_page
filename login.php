2516cf4be888e1457da4ba16510ba9b9b3c05fa4
Subham <banerjee.subham1411@gmail.com>
2023-01-02 19:13:34 +0530

    some changes made

--- login.php
+++ login.php
@@ -29,10 +29,12 @@ if (isset($_POST['login'])) {
                             <p class="text-white mb-5">Please enter your login and password!</p>
                             <form action="login.php" method="POST">
                                 <div class="form-outline form-white mb-4">
-                                    <input type="email" name="email" id="typeEmailX" class="form-control form-control-lg" placeholder="Email" />
+                                    <input type="email" name="email" id="typeEmailX" class="form-control form-control-lg"  />
+                                    <label class="form-label" for="typeEmailX">Email</label>
                                 </div>
                                 <div class="form-outline form-white mb-4">
-                                    <input type="password" name="password" id="typePasswordX" class="form-control form-control-lg" placeholder="Password" />
+                                    <input type="password" name="password" id="typePasswordX" class="form-control form-control-lg"  />
+                                    <label class="form-label" for="typePasswordX">Password</label>
                                 </div>
                                 <p class="small mb-5 pb-lg-2"><a class="text-white" href="#!">Forgot password?</a></p>
                                 <button class="btn btn-outline-light btn-lg px-5" name="login" type="submit">login</button>
--- my-account.php
+++ my-account.php
@@ -131,18 +131,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Welcome <?= $name ?></title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous" />
-  <link rel="stylesheet" href="assets/css/style.css" />
-</head>
-
 <body>
   <div class="row">
     <div class="col-lg-12  ">
