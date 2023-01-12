94e04df133533e85fc547186dee02e61d83dd741
subham141198 <banerjee.subham1411@gmail.com>
2023-01-12 17:44:08 +0530

    some bugs fixed

--- people.php
+++ people.php
@@ -28,42 +28,42 @@ if (!isset($_COOKIE['login_auth'])) {
           $all_data = retrive_all_data();
 
           $all_added_users = all_added_users($_COOKIE['login_auth']);
-          if(count($all_added_users)>0 && $all_added_users){
+          if (count($all_added_users) > 0) {
             foreach ($all_added_users as $key => $value) {
-              $already_added_user[]  = $value[0];
+              $already_added_user[] = $value[0];
             }
           }
 
           foreach ($all_data as $key => $value) {
             $user_id = $value[0];
-            
-            if (!in_array($user_id , $already_added_user)) {
-            if ($user_id != $_COOKIE['login_auth']) {
-              //echo $user_id;
-              ?>
-              <div class="card card-one m-3">
-                <div class="header">
-                  <div class="avatar">
-                    <img src="uploads/<?= $value[8]; ?>" alt="user" class="profile-photo-lg">
+
+            if (!in_array($user_id, $already_added_user)) {
+              if ($user_id != $_COOKIE['login_auth']) {
+                //echo $user_id;
+                ?>
+                <div class="card card-one m-3">
+                  <div class="header">
+                    <div class="avatar">
+                      <img src="uploads/<?= $value[8]; ?>" alt="user" class="profile-photo-lg">
+                    </div>
                   </div>
+                  <h3><a href="#" class="profile-link">
+                      <?= $value[1]; ?>
+                    </a></h3>
+                  <div class="desc">
+                    <?php if (!check_if_already_added($value[0])) { ?>
+                      <button class="btn btn-primary pull-right add_friend_btn" data-id="<?= $value[0]; ?>">Add Friend</button>
+                    <?php } else { ?>
+                      <button class="btn btn-success pull-right add_friend_btn" data-id="<?= $value[0]; ?>">Pending
+                        Request</button>
+                    <?php } ?>
+                  </div>
+                  <div class="contacts">
+                    <div class="clear"></div>
+                  </div>
+                  <div class="footer"></div>
                 </div>
-                <h3><a href="#" class="profile-link">
-                    <?= $value[1]; ?>
-                  </a></h3>
-                <div class="desc">
-                  <?php if (!check_if_already_added($value[0])) { ?>
-                    <button class="btn btn-primary pull-right add_friend_btn" data-id="<?= $value[0]; ?>">Add Friend</button>
-                  <?php } else { ?>
-                    <button class="btn btn-success pull-right add_friend_btn"
-                      data-id="<?= $value[0]; ?>">Pending Request</button>
-                  <?php } ?>
-                </div>
-                <div class="contacts">
-                  <div class="clear"></div>
-                </div>
-                <div class="footer"></div>
-              </div>
-              <?php
+                <?php
               }
             }
           }
